7f947d9bb969dd44ce7f3cbe4aa1b4176232f79c
Isaicla <correaisai270@gmail.com>
2025-09-26 17:04:30 -0400

    hola

--- mokipen/mekipen.js
+++ mokipen/mekipen.js
@@ -1,14 +1,21 @@
 function seleccionarMascotaJugador()  {
-         let inputColas = dsoccument.getElementById("cola")
-         if(inputColas.checked)
-            alert("seleccionastes a colas")
-} else if (inputElSonbreroDePaja)  {
-        
-}
-   
+      let inputColas=document.getElementById("colas")
+      let inputQuin=document.getElementById("quin")
+      let inputElRocas=document.getElementById("el rocas")
+      
 
+         if(inputColas.checked){
+            alert("seleccionastes a colas")
+         }else if(inputQuin.checked){
+            alert("seleccionaste a Quin")
+         }else if(inputElRocas.checked){
+            alert("seleccionaste a el rocas")
+         }else {
+            alert("escoje una mascota")
+         }
+}    
 function reiniciarJuego() {
-      let buttonMascotasJugador = document.getElementById("button-mascota")
+      let buttonMascotasJugador = document.getElementById("buttonMascotaJugador")
 buttonMascotasJugador.addEventListener("click" , seleccionarMascotaJugador)
 }
 window.addEventListener("load", reiniciarJuego)
