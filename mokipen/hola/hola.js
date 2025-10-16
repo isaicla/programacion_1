56e51c1f47e9c92a0467fd7cadf3f8158e24deac
Isaicla <correaisai270@gmail.com>
2025-10-16 18:39:04 -0400

    hola

--- mokipen/hola/hola.js
+++ mokipen/hola/hola.js
@@ -1,20 +1,42 @@
 function inisiarjuego()  {
-    let bottonMascota = document.getElementById("seleccionar")
+    let bottonMascota =document.getElementById("seleccionar")
 bottonMascota.addEventListener("click",seleccionarMascota)
 }
 
-let spammascota = document.getElementById("nombre")
 
 function seleccionarMascota ()  {
-   if(hipodoge.checked){
-    spammascota.innerHTML = "hipodoge"
-   }else if(capipepo.checked){
-    alert("selecionastes a capipepo")
+    let inputHipodoge =document.getElementById("hipodoge")
+let inputcapipepo=document.getElementById("capipepo")
+let inputRatigueya=document.getElementById("ratiueya")
+let spanMascotaJugador=document.getElementById("hols")
+
+
+   if(inputHipodoge.checked){
+    spanMascotaJugador.innerHTML = "Hipodoge"
+   }else if(inputcapipepo.checked){
+    spanMascotaJugador.innerHTML = "capipepo"
    }else if(ratiueya.checked){
-    alert("selecionastes a ratiueya")
+    spanMascotaJugador.innerHTML = "ratiueya"
    }else {
     alert("selecciona una mascota")
+   } 
+
    }
-   }
+    function seleccionarMascotaEnemigo ()  {
+
+
+
+      }
+    
+        
+      
+        
+   function aleatorio(min,max)   {
+        return Math.floor(Math.random()*(max - min + 1) + min )
+      
+
+          }
+        
+        
 
 window.addEventListener("load",inisiarjuego)
